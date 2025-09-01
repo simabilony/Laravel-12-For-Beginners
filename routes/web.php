4f6352cfdf8a9b3a0d57da8b2c99d04304b4c4ab
Sima Bilony <simabilony@gmail.com>
2025-09-01 18:29:12 +0300

    Add homepage

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/second', function () {
+//     return view('second');
+// });
+
+Route::view('/', 'home');
+
+Route::get('/third', function () {
+    return view('third'); // <-- third.blade.php file doesn't exist
 });
+Route::view('/second', 'second');
