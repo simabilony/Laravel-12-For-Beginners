52579050d99dc79c846c5282323aa054956e3c1a
Sima Bilony <simabilony@gmail.com>
2025-09-08 18:37:46 +0300

    Add Elquent ORM

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,7 +12,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $categories = DB::table('categories')->get();
+        //$categories = DB::table('categories')->get();  //query builder
+        
+        $categories = Category::all();  //Elequent ORM
         return view('home', ['categories' => $categories]);
     }
 
